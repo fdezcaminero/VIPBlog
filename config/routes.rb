660d8b9e50b7e45675e9e706637519e0ca262f98
fdezcaminero <fdezcaminero@gmail.com>
2020-04-17 17:34:20 -0400

    Tags work wonders

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
-
   resources :articles do
     resources :comments
   end
+  resources :tags
+
 end
+
+# Blogger::Application.routes.draw do
+
+#   root to: 'articles#index'
+#   resources :articles do
+#     resources :comments
+#   end
+#   resources :tags
+
+# end
