2660c9763a2dc861ef9a40639f2232c587a275df
fdezcaminero <fdezcaminero@gmail.com>
2020-04-20 16:40:16 -0400

    The paperclip image is working

--- db/migrate/20200416215625_create_articles.rb
+++ db/migrate/20200416215625_create_articles.rb
@@ -5,6 +5,10 @@ class CreateArticles < ActiveRecord::Migration[5.2]
       t.text :body  
 
       t.timestamps
+      t.string :image_file_name
+      t.string :image_content_type
+      t.integer :image_file_size
+      t.datetime :image_updated_at
     end
   end
 end
